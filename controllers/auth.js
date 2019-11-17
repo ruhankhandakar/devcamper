b28e0e3407e1abfc635128505ea380bdb29a0091
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-17 07:47:16 +0530

    store reset token and expire time

--- controllers/auth.js
+++ controllers/auth.js
@@ -65,6 +65,29 @@ exports.getMe = asyncHandler(async (req, res, next) => {
   });
 });
 
+/*
+@desc       Forgot Password
+@route      POST /api/v1/auth/forgotpassword
+@access     Public
+*/
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user)
+    return next(new ErrorResponse("There is no user with that Email", 404));
+
+  // Get Reset Token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user
+  });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -40,9 +41,11 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) {
+    next();
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
 });
 
 // get jwt
@@ -55,6 +58,23 @@ UserSchema.methods.getSignedJwt = function() {
 // check password
 UserSchema.methods.checkPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate Hash password token
+UserSchema.methods.getResetPasswordToken = function() {
+  // Genereate Token
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
+  // Hash a token and set to resetPasswordToken filed.
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set the expire
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 module.exports = mongoose.model("User", UserSchema);
