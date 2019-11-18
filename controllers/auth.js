fad41c8f58630c3a5a4e50cfbe77ce103fb52a9c
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-18 08:36:10 +0530

    logut cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -69,6 +69,24 @@ exports.getMe = asyncHandler(async (req, res, next) => {
 });
 
 /*
+@desc       Logout usr
+@route      GET /api/v1/auth/logout
+@access     Private
+*/
+
+exports.logout = asyncHandler(async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 100),
+    httpOnly: true
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Successfully Logged out"
+  });
+});
+
+/*
 @desc       Forgot Password
 @route      POST /api/v1/auth/forgotpassword
 @access     Public
--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,8 @@ const {
   forgotPassword,
   resetPassword,
   updateDetails,
-  updatePassword
+  updatePassword,
+  logout
 } = require("../controllers/auth");
 const { protect } = require("../middleware/auth");
 
@@ -15,6 +16,7 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.get("/logout", protect, logout);
 router.put("/updatedetails", protect, updateDetails);
 router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
