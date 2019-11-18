bec846963d12c0795212b806f632bd397470928e
Ruhan Khandakar <khandakarruhan@gmail.com>
2019-11-18 08:14:46 +0530

    CRUD functionality for reviews

--- models/Review.js
+++ models/Review.js
@@ -33,4 +33,48 @@ const ReviewSchema = new mongoose.Schema({
   }
 });
 
+// Prevent user from submitting more than one review per bootcamp
+ReviewSchema.index(
+  {
+    bootcamp: 1,
+    user: 1
+  },
+  {
+    unique: true
+  }
+);
+
+// Static method to calculate average of rating
+ReviewSchema.statics.getAverageRating = async function(bootcampId) {
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId }
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageRating: { $avg: "$rating" }
+      }
+    }
+  ]);
+
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Call getAverageRating after Save
+ReviewSchema.post("save", function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+ReviewSchema.post("remove", function() {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model("Review", ReviewSchema);
--- routes/review.js
+++ routes/review.js
@@ -6,16 +6,29 @@ const Review = require("../models/Review");
 const advancedResults = require("../middleware/advanceResult");
 const { protect, authorize } = require("../middleware/auth");
 
-const { getReviews, getReview } = require("../controllers/review");
+const {
+  getReviews,
+  getReview,
+  addReview,
+  updateReview,
+  deleteReview
+} = require("../controllers/review");
 
-router.route("/").get(
-  advancedResults(Review, {
-    path: "bootcamp",
-    select: "name description"
-  }),
-  getReviews
-);
+router
+  .route("/")
+  .get(
+    advancedResults(Review, {
+      path: "bootcamp",
+      select: "name description"
+    }),
+    getReviews
+  )
+  .post(protect, authorize("user", "admin"), addReview);
 
-router.route("/:id").get(getReview);
+router
+  .route("/:id")
+  .get(getReview)
+  .put(protect, authorize("user", "admin"), updateReview)
+  .delete(protect, authorize("user", "admin"), deleteReview);
 
 module.exports = router;
